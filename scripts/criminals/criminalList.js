c0f891bfe421824e624fae5ad277068541104731
TKomrij <tatiane.b.komrij@gmail.com>
2020-12-07 09:03:26 -0600

    made filter work

--- scripts/criminals/criminalList.js
+++ scripts/criminals/criminalList.js
@@ -1,16 +1,49 @@
 import { getCriminals, useCriminals } from './criminalProvider.js'
 import {Criminal} from './criminal.js'
+import { useConvictions } from '../convictions/convictionProvider.js'
+
+const criminalElement = document.querySelector(".criminalsContainer")
+const eventHub = document.querySelector(".container")
+
+// Listen for the custom event you dispatched in ConvictionSelect
+eventHub.addEventListener('crimeChosen', event => {
+  // Use the property you added to the event detail.
+  if (event.detail.crimeThatWasChosen !== "0"){
+      /*
+          Filter the criminals application state down to the people that committed the crime
+      */
+
+      const crimes = useConvictions()
+      const crime = crimes.find( (crime) => crime.id === parseInt(event.detail.crimeThatWasChosen))
+
+      const criminals = useCriminals()
+      const matchingCriminals = criminals.filter( (criminal) => criminal.conviction === crime.name)
+    
+      render(matchingCriminals)
+
+   
+
+      /*
+          Then invoke render() and pass the filtered collection as
+          an argument
+      */
+  }
+})
 
 export const criminalList = () => {
   getCriminals().then( 
     () => {
-      const contentElement = document.querySelector(".criminalsContainer")
-      const allTheCriminals = useCriminals()
-      for (const criminalObject of allTheCriminals) {
-        const criminalHTML = Criminal(criminalObject)
-      // Add to the existing HTML in the content element
-      contentElement.innerHTML += criminalHTML
-    }
+      let perps = useCriminals()
+      render(perps)
+      
     }
   )
+}
+
+const render = (criminals) => {
+  let criminalCards = []
+  for (const perp of criminals) {
+    criminalCards.push(Criminal(perp))
+  }
+  criminalElement.innerHTML = criminalCards.join("")
 }
--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,19 @@ import {ConvictionSelect} from './convictions/convictionSelect.js'
 
 criminalList()
 ConvictionSelect()
+
+
+// What feature are we implementing?
+// Filter our criminals by the crimes committed
+
+
+// What tasks do we need to complete to implement the feature?
+// 1. Filter through the criminals data by matching the crime that has been selected 
+// 2. listen for the slection of a crime and capture the chosen value
+// 3. Use the selected value to filter the criminal data
+// 4. Update the DOM with the filtered criminal data
+
+
+// Which modules are involved?
+// 1. criminalList
+// 2. convictionSelect
